143b7bc4c40a271f91263a623c6ff7b289596c03
Sara Costa <sara2006costa@gmail.com>
2024-01-03 09:08:37 -0300

    ::tada: initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("input-field")
+const addBtn = document.getElementById("add-button")
+
+addBtn.addEventListener("click", function(){
+    let inputValue = input.value
+    console.log(inputValue)
+})
