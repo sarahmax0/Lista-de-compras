ba49698d4fe6faca49c5f3a11028327d0da986c1
Sara Costa <sara2006costa@gmail.com>
2024-01-04 23:06:59 -0300

    Append li to ul

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const input = document.getElementById("input-field")
-const addBtn = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-addBtn.addEventListener("click", function(){
-    let inputValue = input.value
-    console.log(inputValue)
+const appSettings = {
+    databaseURL: "https://realtime-database-abb76-default-rtdb.firebaseio.com/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
+    inputFieldEl.value = " "
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    
 })
